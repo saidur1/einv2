85199d62a4ff2e90e39b6199de00f6cb5937cd3c
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-11-26 16:40:43 +0600

    added: schedule page

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,5 +1,15 @@
 const Schedule = () => {
-  return <div>Schedule</div>;
+  return (
+    <div className="container my-[50px]">
+      <h1 className="text-[44px] text-[#2F2F2F] max-w-[930px] mx-auto text-center">
+        Schedule Your Breakthrough Strategy Session With Sam Right Now
+      </h1>
+
+      <div className="shadow-md w-full h-[500px] border-[1px] mt-10 flex justify-center items-center text-gray-400">
+        Calender will be load later
+      </div>
+    </div>
+  );
 };
 
 export default Schedule;
